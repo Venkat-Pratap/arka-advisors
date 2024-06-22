ce9411dbdcc4e4164539b7989e0eef7ee6e8db81
Venkat-Pratap <pratap.prabhakar@gmail.com>
2024-06-22 21:19:51 +0530

    test message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Test page
-    </main>
-  );
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <div><Image src="/logo-dark.png" alt="Arka Logo" width={172} height={16} /></div>
+      <div className="mt-2">Test page</div>
+</main>
+)
 }
